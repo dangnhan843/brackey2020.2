06a4cbf1961802bf1da82f115b96b09404d5ad17
Nhan Nguyen <dangnhan843@email.arizona.edu>
2020-08-04 05:34:08 -0700

    1/2 graphic

---
+++ brackey2020.2/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerT;
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    void Start()
+    {
+        _cameraOffset = transform.position - playerT.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = playerT.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+    }
+}
---
+++ brackey2020.2/Assets/Scripts/Rewind.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rewind : MonoBehaviour
+{
+    //public Transform rewindPoint;
+    //public Transform player;
+    public float timeRemaining = 2;
+    bool rewindCountdown = false;
+    bool rewind = false;
+    private Vector3 rewindPosition;
+    // Update is called once per frame
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        //Debug.Log(playerT.position);
+
+        if (Input.GetButtonDown("Fire1") && rewindCountdown == false)
+        {
+            rewindCountdown = true;
+            rewindPosition = transform.position;
+            //Debug.Log(transform.position);
+        }
+
+        if (rewindCountdown == true)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                Debug.Log(timeRemaining);
+            }
+        }
+
+        if (timeRemaining < 0)
+        {
+            rewind = true;
+            rewindCountdown = false;
+            timeRemaining = 2;
+        }
+
+        if (rewind == true)
+        {
+            rewindChorno();
+            Debug.Log("here" + rewind);
+        }
+        
+    }
+
+    public void rewindChorno()
+    {
+        this.transform.position = rewindPosition;
+        rewind = false;
+
+    }
+}
